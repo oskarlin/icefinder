af84699fa48f6a90d13a793d12ee899e511d41f5
Oskar Karlin <oskar@thatswe.com>
2013-10-21 12:16:37 +0200

    Clean up and some commenting

--- core/icefinder.sh
+++ core/icefinder.sh
@@ -2,44 +2,51 @@
 # icefinder.sh version 2.0b
 # usage: sh icefinder.sh 
 
-# SATELLITE="$1"
-# FILE_TODAY="$1"
-# CAT_NAME="$3"
+FILE_TODAY=$(date +"%Y%j") # use this if you want todays files
 
-FILE_TODAY=$(date +"%Y%j")
-
-# FILE_TODAY="2013293"
+# FILE_TODAY="2013293"  # use this line if you want a specific date
 
 
-### Downloading JPG from NASA.
+### Downloading two JPG files from Earthdata.nasa.gov (extracted from https://earthdata.nasa.gov/labs/worldview/ )
 
 wget -O 1-2-1.jpg "http://map2.vis.earthdata.nasa.gov/imagegen/index.php?TIME=$FILE_TODAY&extent=4.0,55.0,31.0,70.0&epsg=4326&layers=MODIS_Terra_SurfaceReflectance_Bands121&format=image/jpeg&width=12288&height=6827"
 
 wget -O 3-6-7.jpg "http://map2.vis.earthdata.nasa.gov/imagegen/index.php?TIME=$FILE_TODAY&extent=4.0,55.0,31.0,70.0&epsg=4326&layers=MODIS_Terra_CorrectedReflectance_Bands367&format=image/jpeg&width=12288&height=6827"
 
+# Reproject the downloaded files to web mercator and TIF
+
 gdalwarp -s_srs "+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs" -t_srs "+proj=merc +lon_0=0 +k=1 +x_0=0 +y_0=0 +a=6378137 +b=6378137 +units=m +no_defs " -r bilinear 1-2-1.jpg 1-2-1_3857.tif
 
 gdalwarp -s_srs "+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs" -t_srs "+proj=merc +lon_0=0 +k=1 +x_0=0 +y_0=0 +a=6378137 +b=6378137 +units=m +no_defs " -r bilinear 3-6-7.jpg 3-6-7_3857.tif
 
-# create big image
+
+### Create a big JPG with the two layers using make_satellite.py with the imagery.xml script
 
 python make_satellite.py
 
-# generate tiles
 
-MAPNIK_MAP_FILE="image.xml"  MAPNIK_TILE_DIR="tiles$FILE_TODAY"  python generate_tiles_imagery.py
+# I have included the two steps above (gdalwarp and make_satellite.py) to speed up the actual tile generation (below)
 
-# moving and renaming the catalog to right name
+### generate tiles
 
-# mv /Users/oskarlin/Sites/modisstor "/Users/oskarlin/Sites/icefinder/$SATELLITE.$FILE_TODAY"
+MAPNIK_MAP_FILE="image.xml"
+MAPNIK_TILE_DIR="tiles$FILE_TODAY" 
+python generate_tiles_imagery.py
 
-# upload to FTP
+### moving and renaming the catalog to right name
 
-# ncftpput -r 10 -R -v -z -F -u "skridskokartan.se" -p "zdf092T" ftp.skridskokartan.se /icefinder "$SATELLITE.$FILE_TODAY"
-# ncftpput -r 10 -R -v -z -F -u "skridskokartan.se" -p "zdf092T" ftp.skridskokartan.se /icefinder "$SATELLITE.$FILE_TODAY"
-# ncftpput -r 10 -R -v -z -F -u "skridskokartan.se" -p "zdf092T" ftp.skridskokartan.se /icefinder "$SATELLITE.$FILE_TODAY"
-# ncftpput -r 10 -R -v -z -F -u "skridskokartan.se" -p "zdf092T" ftp.skridskokartan.se /icefinder "$SATELLITE.$FILE_TODAY"
-# ncftpput -r 10 -R -v -z -F -u "skridskokartan.se" -p "zdf092T" ftp.skridskokartan.se /icefinder "$SATELLITE.$FILE_TODAY"
+## Here we put the code for getting the catalog into the right place ##
 
-# rm 3-6-7_3857.tif
-# rm 1-2-1_3857.tif
+## Here we put the code for updating the database with todays date ##
+
+
+### Renaming combine file (as backup)
+
+mv combine.jpg combine.$FILE_TODAY.jpg
+
+### Removing all used files
+
+rm 1-2-1.jpg
+rm 3-6-7.jpg
+rm 3-6-7_3857.tif
+rm 1-2-1_3857.tif
