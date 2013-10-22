e047f80d79a0b2e4b23147dcf1b3b3c8f563fe86
Oskar Karlin <oskar@thatswe.com>
2013-10-23 00:22:04 +0200

    fixfix

--- core/make_satellite.py
+++ core/make_satellite.py
@@ -2,7 +2,8 @@
 import mapnik
 stylesheet = 'imagery.xml'
 image = 'combined.jpg'
-m = mapnik.Map(8853, 10919)
+m = mapnik.Map(4426, 5459)
+# m = mapnik.Map(8853, 10919)
 mapnik.load_map(m, stylesheet)
 m.zoom_all() 
 mapnik.render_to_file(m, image)
