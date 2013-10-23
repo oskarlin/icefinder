cba9c09f83f15db8b0890a11bd08ffa3117dca38
Oskar Karlin <oskar@thatswe.com>
2013-10-23 12:50:41 +0200

    Create big file straight from downloaded JPG:s

--- core/make_satellite.py
+++ core/make_satellite.py
@@ -2,8 +2,7 @@
 import mapnik
 stylesheet = 'imagery.xml'
 image = 'combined.jpg'
-m = mapnik.Map(4426, 5459)
-# m = mapnik.Map(8853, 10919)
+m = mapnik.Map(8853, 10919)
 mapnik.load_map(m, stylesheet)
 m.zoom_all() 
 mapnik.render_to_file(m, image)
