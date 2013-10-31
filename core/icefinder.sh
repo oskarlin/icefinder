cd57e5316a3cb0a3b89caae3ea6d596aadbe8d17
Oskar Karlin <oskar@thatswe.com>
2013-10-31 21:39:40 +0100

    maybe fix

--- core/icefinder.sh
+++ core/icefinder.sh
@@ -36,7 +36,10 @@ gdal_translate combined.jpg combined.tif -co "TILED=YES" # This is also for spee
 ### generate tiles from the combine.tif.
 ### Going back to Mapnik since gdal2tiles makes ugly tiles (no nice resampling)
 
-MAPNIK_MAP_FILE="image.xml" MAPNIK_TILE_DIR="$TILEPATH/$DATE_NASA" MAPNIK_MINZOOM="5" MAPNIK_MAXZOOM="11" python generate_tiles_imagery.py
+MAPNIK_MAP_FILE="image.xml" MAPNIK_TILE_DIR="$TILEPATH/$DATE_NASA" MAPNIK_MINZOOM="5" MAPNIK_MAXZOOM="9" python generate_tiles_imagery.py
+
+MAPNIK_MAP_FILE="image.xml" MAPNIK_TILE_DIR="$TILEPATH/$DATE_NASA" MAPNIK_MINZOOM="10" MAPNIK_MAXZOOM="11" python generate_tiles_imagery.py
+
 
 # python gdal2tiles_jpg.py --tile-format="jpeg" -r bilinear -z 5-11 -s "+proj=merc +lon_0=0 +k=1 +x_0=0 +y_0=0 +a=6378137 +b=6378137 +units=m +no_defs" combined.jpg $TILEPATH/$DATE_NASA
 
