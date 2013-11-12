9dbba1275a44323cb53728bb85a5666f861ae32d
Oskar Karlin <oskar@thatswe.com>
2013-11-12 16:20:03 +0100

    Fix for new generate server

--- core/icefinder.sh
+++ core/icefinder.sh
@@ -25,21 +25,22 @@ wget -O 3-6-7.jpg "http://map2.vis.earthdata.nasa.gov/imagegen/index.php?TIME=$D
 
 ### Create a big JPG with the two layers using make_satellite.py with the imagery.xml script
 
-echo "generating combined image..."
-python make_satellite.py   # I have included make_satellite.py to speed up the actual tile generation (below)
+### echo "generating combined image..."
+### python make_satellite.py   # I have included make_satellite.py to speed up the actual tile generation (below)
 
 ### Create a TIF which has the content tiled.
 
 echo "converting to tiled TIF" 
-gdal_translate combined.jpg combined.tif -co "TILED=YES" # This is also for speeding up tile making
+
+gdal_translate 1-2-1.jpg 1-2-1.tif -co "TILED=YES"
+gdal_translate 3-6-7.jpg 3-6-7.tif -co "TILED=YES"
+
+### gdal_translate combined.jpg combined.tif -co "TILED=YES" # This is also for speeding up tile making
 
 ### generate tiles from the combine.tif.
 ### Going back to Mapnik since gdal2tiles makes ugly tiles (no nice resampling)
 
-MAPNIK_MAP_FILE="image.xml" MAPNIK_TILE_DIR="$TILEPATH/$DATE_NASA" MAPNIK_MINZOOM="5" MAPNIK_MAXZOOM="9" python generate_tiles_imagery.py
-
-MAPNIK_MAP_FILE="image.xml" MAPNIK_TILE_DIR="$TILEPATH/$DATE_NASA" MAPNIK_MINZOOM="10" MAPNIK_MAXZOOM="11" python generate_tiles_imagery.py
-
+MAPNIK_MAP_FILE="image_cloud.xml" MAPNIK_TILE_DIR="$TILEPATH/$DATE_NASA" MAPNIK_MINZOOM="5" MAPNIK_MAXZOOM="11" python generate_tiles_imagery.py
 
 # python gdal2tiles_jpg.py --tile-format="jpeg" -r bilinear -z 5-11 -s "+proj=merc +lon_0=0 +k=1 +x_0=0 +y_0=0 +a=6378137 +b=6378137 +units=m +no_defs" combined.jpg $TILEPATH/$DATE_NASA
 
