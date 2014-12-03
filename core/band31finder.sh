d5a6ebec21e734a6dad335324580c25ea84cef5d
Oskar Karlin <oskar@thatswe.com>
2014-12-03 15:20:36 +0100

    updates

--- core/band31finder.sh
+++ core/band31finder.sh
@@ -9,8 +9,10 @@ DATE_NASA=$(date +"%Y%j")
 
 # download the image
 
-wget -O image.tif "http://map2.vis.earthdata.nasa.gov/imagegen/index.php?TIME= $DATE_NASA&extent=4.0,55.0,31.0,70.0&epsg=4326&layers=MODIS_Terra_Land_Surface_Temp_Day&format=image/tiff&width=12288&height=6827"
+echo "laddar ner image..."
+echo $DATE_NASA
 
+wget -O image.tif "http://map2.vis.earthdata.nasa.gov/imagegen/index.php?TIME=$DATE_NASA&extent=4.0,55.0,31.0,70.0&epsg=4326&layers=MODIS_Terra_Land_Surface_Temp_Day&format=image/tiff&width=12288&height=6827"
 
 gdal_translate -b 1 image.tif image_red.tif
 gdal_translate -b 2 image.tif image_green.tif
@@ -20,7 +22,9 @@ gdaldem color-relief image_green.tif -alpha green_colortable.txt image_green_rgb
 
 python band31_combine.py
 
+gdal_translate image_combined.png $DATE_NASA.combined.tif
 
+# MAPNIK_MAP_FILE="band31.xml" MAPNIK_TILE_DIR="$DATE_NASA" MAPNIK_MINZOOM="5" MAPNIK_MAXZOOM="9" python generate_tiles_band31.py
 
 ## gdal_translate /root/git/icefinder/core/1-2-1.jpg /root/git/icefinder/core/1-2-1.tif -co "TILED=YES"
 ## gdal_translate /root/git/icefinder/core/3-6-7.jpg /root/git/icefinder/core/3-6-7.tif -co "TILED=YES"
